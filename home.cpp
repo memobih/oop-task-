3079a0ef3660e13e6de17c0072ad660879b6d612
mohamed_abdo <91019053+memobih@users.noreply.github.com>
2022-11-06 07:35:09 +0200

    Create home.cpp

---
+++ home.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> // بسم  الله الرحمن الرحيم
+using namespace std;
+#define ll long long
+class home
+{      private :
+     int room; vector<string>color;
+     string color_kitchen;
+     string color_bathroom;
+     public:
+     void set(int rooms); // set the numper of room;
+     void set_colorroom(); // set the colours of rooms;
+     set_kitchencolor(string k);
+     set_bathroomcolor(string b);
+
+};
+int main() {
+
+}
