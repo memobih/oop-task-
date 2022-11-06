dc443a4ecb5ab8ffa9dca59f8d5a490ca15af63a
mohamed_abdo <91019053+memobih@users.noreply.github.com>
2022-11-06 07:13:32 +0200

    Create data.cpp

---
+++ data.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> // بسم  الله الرحمن الرحيم
+using namespace std;
+#define ll long long
+struct  data_time
+{
+ int hour;
+ int minuit;
+ int second;
+
+
+};
+struct data_date
+{
+    int month;
+    int day ;
+    int year ;
+
+};
+struct data{
+
+data_date date ;
+data_time time ;
+data (data_date date , data_time time  ) {
+
+	}
+
+};
+int main() {
+
+}
