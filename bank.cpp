9d236ed6c4e5d778df64b3d56f461f5a4a91bd75
mohamed_abdo <91019053+memobih@users.noreply.github.com>
2022-11-06 13:23:27 +0200

    Create bank.cpp

---
+++ bank.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> // بسم  الله الرحمن الرحيم
+using namespace std;
+#define ll long long
+class bank
+{
+    string name ;
+    string phone ;
+    string address;
+     int month_birth ;
+     int day_birth;
+     int year_birth ;
+
+ public :
+    string  get_birth(); // return  birth as string
+    void  get_data ( ); // desplay  data of costumer
+};
+int main() {
+
+}
